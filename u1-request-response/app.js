deafad07876139aa242a4ecd9715dafe8d16ec34
Thanawat-Samat <86312282+Thanawat-Samat@users.noreply.github.com>
2021-06-24 16:47:25 +0700

    [U1L] HTML & JSON responses

--- u1-request-response/app.js
+++ u1-request-response/app.js
@@ -66,6 +66,91 @@ app.get(`/status-400` ,(req,res) => {
         `)
     })
 
+//LabIndex
+//localhost:3000/labs
+    app.get(`/labs` ,(req,res) => {
+        res.send(`
+        <html>
+            <body>
+                <li>1. <a href="/profiles/bank">/profiles/bank</a></li>
+                <li>2. <a href="/api/profiles/bank">/api/profiles/bank</a></li>
+                <li>3. <a href="/cat-simulator">/cat-simulator</a></li>
+                <li>4. <a href="/admin/grades">/admin/grades</a> AND <a href="/api/grades">/api/grades</a> AND <a href="/boredom">/boredom</a></li>
+            </body>
+        </html>
+        `)
+    })    
+
+//localhost:3000/profiles/bank
+    app.get(`/profiles/bank`,(req,res) => {
+        res.send(`
+        <html><body>
+            <h1>Thanawat Samat<h1>
+            <h4> Nickname: Bank <br>
+                Age: 20 <br>
+                Like: color black&blue. 
+                Motto: nothing is impossible
+            <h4>
+        </body></html>
+        `)
+    })
+
+    app.get(`/api/profiles/bank`,(req,res) => {
+        res.send(
+        {
+            "name": "Thanawat Samat",
+            "Nickname": "Bank",
+            "Age": "20",
+            "Like": ["color black&blue",
+                "nature",
+                "socializing"
+            ],
+            "motto": "nothing is impossible"
+        }
+        )
+    })
+
+//localhost:3000/cat-simulator
+    app.get(`/cat-simulator`,(req,res) => {
+        res.send(`
+        <html>
+            <head><link rel="stylesheet" href="/styles.css"></head>
+                <body class="cats">
+                    <a href="/cat-result">Pet the cat</a><br>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/British_shorthair_cat.jpg/800px-British_shorthair_cat.jpg">
+                </body>
+        </html>
+        `)
+    })
+
+//localhost:3000/cat-result
+    app.get(`/cat-result`,(req,res) => {
+        res.send(`
+        <html>
+            <head><link rel="stylesheet" href="/styles.css"></head>
+                <body class="cats">
+                    <a href="/cat-simulator">Pet the cat</a><br>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Hannibal_Poenaru_-_Nasty_cat_%21_%28by-sa%29.jpg/755px-Hannibal_Poenaru_-_Nasty_cat_%21_%28by-sa%29.jpg">
+                </body>
+        </html>
+        `)
+    })
+
+//localhost:3000/admin/grades
+    app.get(`/admin/grades` ,(req,res) => {
+        res.sendStatus(401)
+    })
+
+//localhost:3000/api/grades
+    app.get(`/api/grades` ,(req,res) => {
+        res.status(401).send(`Invalid OAuth token`)
+    })
+
+//localhost:3000/boredom
+    app.get(`/boredom` ,(req,res) => {
+        res.status(404).send(`Sorry - we don't that here!`)
+    })    
+
 //Start server กำหนดpostแทนในlo post
 const POST = 3000
 app.listen(POST, () => console.log(`listening on http://localhost:${POST}`))
