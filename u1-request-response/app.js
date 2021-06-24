b17ac3335018375be09fd5f419c79adee1af9a58
AreeyaC <86303607+AreeyaC@users.noreply.github.com>
2021-06-24 16:49:47 +0700

    [U1L] HTML & JSON responses Labs1

--- u1-request-response/app.js
+++ u1-request-response/app.js
@@ -49,7 +49,7 @@ app.get('/status-200', (req, res) => {
 
 // localhost:3000/status2
 app.get('/status2', (req, res) => {
-  res.status(500).send('Oh no, somthing went wrong')
+  res.status(500).send('Oh no, something went wrong')
   })
 
 // localhost:3000/html-pic
@@ -66,3 +66,83 @@ app.get('/html-pic', (req, res) => {
 const PORT = 3000
 app.listen(PORT, () => console.log(
   `listening on http://localhost:${PORT}`))
+
+// Labs
+app.get('/labs' , (req, res) => {
+res.send(`
+<html><body>
+  <li>1. <a href="/profiles/areeya">/profiles/areeya</a></li>
+  <li>2. <a href="/api/profiles/areeya">/api/profiles/areeya</a></li>
+  <li>3. <a href="/cat-simulator">/cat-simulator</a></li>
+  <li>4. <a href="/admin/grades">/admin/grades</a> AND <a href="/api/grades">/api/grades</a> AND <a href="/boredom">/boredom</a></li>
+</body></html>
+`)
+})  
+
+// localhost:3000/profiles/areeya
+app.get('/profiles/areeya', (req, res) => {
+  res.send(`
+  <html><body>
+  <h1>Areeya Chanthawong</h1>
+      <h2> Nickname : mook <br>
+        Age: 22 <br>
+        Likes: Graphic,Lean Web 
+      </h2>
+    
+</body></html>
+  `)
+})
+
+// localhost:3000/api/profiles/areeya
+app.get('/api/profiles/areeya', (req, res) => {
+  res.send(`
+  "name": "Areeya Chanthawong",
+  "nickname": "Mook",
+  "like": [
+    "Graphic",
+    "Lean Web"
+  ]
+`)
+})
+
+//localhost:3000/cat-simulator
+app.get('/cat-simulator', (req,res) => {
+  res.send(`
+  <html>
+  <head><link rel="stylesheet" href="/styles.css"></head>
+      <body class="cats">
+              <a href="/cat-result">Pet the cat</a><br>
+              <img src="/images/catt.jpg"/>
+          </body>
+  </html>
+  `)
+})
+
+//localhost:3000/cat-result
+app.get('/cat-result', (req,res) => {
+  res.send(`
+  <html>
+  <head><link rel="stylesheet" href="/styles.css"></head>
+                <body class="cats">
+              <a href="/cat-simulator">Pet the cat</a><br>
+              <img src="/images/catty.jpg"/>
+          </body>
+  </html>
+  `)
+})
+
+//localhost:3000/admin/grades
+app.get('/admin/grades', (req,res) => {
+  res.sendStatus(401)
+})
+
+
+//localhost:3000/api/grades
+app.get('/api/grades', (req,res) => {
+  res.status(401).send('Invalid OAuth token')
+})
+
+//localhost:3000/boredom
+    app.get('/boredom', (req,res) => {
+        res.status(404).send(`Sorry - we don't that here!`)
+    })    
