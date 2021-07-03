59bd671af011bdc1ea3a070de0d75b9f8eadf013
OilWanpen <wanpenp62@nu.ac.th>
2021-07-03 21:32:01 +0700

    [U2L] Routing & Controllers Labs

--- u2-routing-controllers/_services/fakedb.js
+++ u2-routing-controllers/_services/fakedb.js
@@ -41,8 +41,22 @@ const findUserBySearch = (search) => {
     user => user.name.toLowerCase().includes(lowerSearch))
 }
 
+const products = {
+  lenovo: {
+
+  },
+  asus: {
+
+  }
+}
+
+const findProducts = (q) => {
+  return products[q]
+}
+
 module.exports = {
   findUser,
   findUserByUsername,
-  findUserBySearch
+  findUserBySearch,
+  findProducts
 }
---
+++ u2-routing-controllers/features/rgbController.js
@@ -0,0 +1,19 @@
+const getRGB = (req, res) => {
+  const r = parseInt(req.query.r) || 0
+  const g = parseInt(req.query.g) || 0
+  const b = parseInt(req.query.b) || 0
+  const average = Math.round((r + g + b) / 3)
+
+  if (average < 128) {
+    res.send(`<html><body style="background-color: rgb(${r},${g},${b});">
+    <font color=white> red: ${r}, green: ${g}, blue: ${b}<br>
+    avg: ${average} </font></body></html>`)
+  } else {
+    res.send(`<html><body style="background-color: rgb(${r},${g},${b});">
+    red: ${r}, green: ${g}, blue: ${b}<br>
+    avg: ${average} </font></body></html>`)
+  }
+}
+module.exports = {
+  getRGB
+}
