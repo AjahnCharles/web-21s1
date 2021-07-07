8c63fb628786ada46d2e0b458cff49013aa605ce
OilWanpen <wanpenp62@nu.ac.th>
2021-07-07 15:57:43 +0700

    [U3] Course Units & Students pages

--- u3-views/app.js
+++ u3-views/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const expHbs = require('express-handlebars')
 const { index } = require('./features/indexController')
-const { courseDetails } = require('./features/courseController')
+const { courseDetails, courseUnits, courseStudents } = require('./features/courseController')
 
 const app = express()
 
@@ -20,6 +20,8 @@ app.use(express.static('public'))
 // Routes
 app.get('/', index)
 app.get('/courses/:code', courseDetails)
+app.get('/courses/:code/units', courseUnits)
+app.get('/courses/:code/students', courseStudents)
 
 const PORT = 3000
 app.listen(PORT, () => console.log(`Listening: http://localhost:${PORT}`))
--- u3-views/features/courseController.js
+++ u3-views/features/courseController.js
@@ -9,6 +9,24 @@ const courseDetails = (req, res) => {
   res.render('course-details', { course: course })
 }
 
+const courseUnits = (req, res) => {
+  const courseCode = req.params.code
+  const course = getCourse(courseCode)
+
+  // left units = name in handlebars
+  // right course.units = expression
+  res.render('course-units', { units: course.units })
+}
+
+const courseStudents = (req, res) => {
+  const courseCode = req.params.code
+  const course = getCourse(courseCode)
+
+  res.render('course-students', { course: course })
+}
+
 module.exports = {
-  courseDetails
+  courseDetails,
+  courseUnits,
+  courseStudents
 }
