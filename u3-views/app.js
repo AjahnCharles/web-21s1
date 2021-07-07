c728d7d5b6cae9c3a73631029be72fe9a1dd20a4
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-07-07 14:23:00 +0700

    [U3] Course Units & Students pages

--- u3-views/app.js
+++ u3-views/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const expHbs = require('express-handlebars')
-const { courseDetails } = require('./features/courseController')
+const { courseDetails, courseUnits, courseStudents } = require('./features/courseController')
 const { index } = require('./features/indexController')
 
 const app = express()
@@ -20,6 +20,8 @@ app.use(express.static('public'))
 // Routes
 app.get('/', index)
 app.get('/courses/:code', courseDetails)
+app.get('/courses/:code/units', courseUnits)
+app.get('/courses/:code/students', courseStudents)
 
 const PORT = 3000
 app.listen(PORT, () => console.log(`Listening: http://localhost:${PORT}`))
