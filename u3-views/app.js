ecb1d50e8598601e9ed5bfb0d449f9e6a261f885
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-07-07 14:06:05 +0700

    [U3] Configure handlebars

--- u3-views/app.js
+++ u3-views/app.js
@@ -1,6 +1,18 @@
 const express = require('express')
+const expHbs = require('express-handlebars')
+const { courseDetails } = require('./features/courseController')
+const { index } = require('./features/indexController')
 
 const app = express()
+
+// Templates
+app.set('views', './views')
+app.set('view engine', 'hbs')
+app.engine('hbs', expHbs({
+  extname: '.hbs',
+  defaultLayout: false,
+  partialsDir: ['./views/partials', './views/layouts']
+}))
 
 // Middleware
 app.use(express.static('public'))
