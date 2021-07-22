29dd3f1ec9faab85b20d90372b8ebf356c1ef107
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-07-22 12:17:18 +0700

    [U5L] Create COVID record

--- u5-rest-apis/features/covid-controller.js
+++ u5-rest-apis/features/covid-controller.js
@@ -1,4 +1,5 @@
 const { db } = require('../_services/firebase-admin-initialized')
+const { firestore } = require('firebase-admin')
 
 const readCovidRecords = async (req, res) => {
   try {
@@ -43,7 +44,49 @@ const readCovidRecord = async (req, res) => {
   }
 }
 
+const createCovidRecord = async (req, res) => {
+  try {
+    /*********************************/
+    /* Version 1: Only update latest */
+    /*********************************/
+
+    // // 1. Inputs
+    // const { stateId, date, cases, casesNew, vaccineOne, vaccineOnePercent, vaccineComplete, vaccineCompletePercent } = req.body
+    // const record = { stateId, date: firestore.Timestamp.fromMillis(date), cases, casesNew, vaccineOne, vaccineOnePercent, vaccineComplete, vaccineCompletePercent }
+
+    // // 2. Query
+    // const query = db.collection('covid-latest').doc(stateId).set(record, { merge: true })
+
+    // // 3. Response
+    // await query
+    // res.sendStatus(201)
+
+    /**********************************/
+    /* Version 2: Also update history */
+    /**********************************/
+
+    // 1. Inputs
+    const { stateId, date, cases, casesNew, vaccineOne, vaccineOnePercent, vaccineComplete, vaccineCompletePercent } = req.body
+    const record = { stateId, date: firestore.Timestamp.fromMillis(date), cases, casesNew, vaccineOne, vaccineOnePercent, vaccineComplete, vaccineCompletePercent }
+    const historyEntry = { date: firestore.Timestamp.fromMillis(date), cases, casesNew, vaccineOne, vaccineOnePercent, vaccineComplete, vaccineCompletePercent }
+    const historyUpdate = { history: firestore.FieldValue.arrayUnion(historyEntry) }
+
+    // 2. Query
+    const batch = db.batch()
+    batch.set(db.collection('covid-latest').doc(stateId), record, { merge: true })
+    batch.set(db.collection('covid-history').doc(stateId), historyUpdate, { merge: true })
+
+    // 3. Response
+    await batch.commit()
+    res.sendStatus(201)
+  } catch (err) {
+    console.error(err)
+    res.sendStatus(500)
+  }
+}
+
 module.exports = {
   readCovidRecords,
-  readCovidRecord
+  readCovidRecord,
+  createCovidRecord
 }
