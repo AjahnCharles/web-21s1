72fe768155242e34759671270d1d4fb164179517
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-07-29 15:29:37 +0700

    [U5L] Improved create/update again...
    - Create is now idempotent (can be called multiple times with the same end result)
    - Update is rejected if it would actually create a new record
    - More consistent code style
    - Better comments

--- u5-rest-apis/features/covid-controller.js
+++ u5-rest-apis/features/covid-controller.js
@@ -15,6 +15,7 @@ const readCovidRecords = async (req, res) => {
       .map(doc => doc.data())
       .map(({ date, stateId, stateName, cases, casesNew, vaccineOne, vaccineOnePercent, vaccineComplete, vaccineCompletePercent }) =>
         ({ date: date.toMillis(), stateId, stateName, cases, casesNew, vaccineOne, vaccineOnePercent, vaccineComplete, vaccineCompletePercent }))
+
     res.json({ result: 'ok', payload, count: payload.length })
   } catch (err) {
     console.error(err)
@@ -37,6 +38,7 @@ const readCovidRecord = async (req, res) => {
     const data = snapshot.data()
     const history = data.history.map(({ date, cases, casesNew, vaccineOne, vaccineOnePercent, vaccineComplete, vaccineCompletePercent }) => ({ date: date.toMillis(), cases, casesNew, vaccineOne, vaccineOnePercent, vaccineComplete, vaccineCompletePercent }))
     const payload = { stateId, stateName: data.stateName, history }
+
     res.json({ result: 'ok', payload })
   } catch (err) {
     console.error(err)
@@ -52,9 +54,9 @@ const createCovidRecord = async (req, res) => {
     // 1. Inputs
     const { stateId, date: dateMillis, cases, casesNew, vaccineOne, vaccineOnePercent, vaccineComplete, vaccineCompletePercent } = req.body
     const date = firestore.Timestamp.fromMillis(dateMillis)
-    const latestUpdate = { stateId, date, cases, casesNew, vaccineOne, vaccineOnePercent, vaccineComplete, vaccineCompletePercent }
 
     // 2. Query
+    const latestUpdate = { stateId, date, cases, casesNew, vaccineOne, vaccineOnePercent, vaccineComplete, vaccineCompletePercent }
     const query = db.collection('covid-latest').doc(stateId).set(latestUpdate, { merge: true })
 
     // 3. Response
@@ -65,43 +67,54 @@ const createCovidRecord = async (req, res) => {
     res.sendStatus(500)
   }
 
-  /**********************************/
-  /* Version 2: Check & update both */
-  /**********************************/
+  /*************************************/
+  /* Version 2: Validate & update both */
+  /*************************************/
   // try {
   //   // 1. Inputs
   //   const { stateId, date: dateMillis, cases, casesNew, vaccineOne, vaccineOnePercent, vaccineComplete, vaccineCompletePercent } = req.body
   //   const date = firestore.Timestamp.fromMillis(dateMillis)
 
-  //   // 2. Query
-  //   const query = db.runTransaction(async t => {
-  //     // Read data from covid-latest & covid-history
+  //   // 2. Query AND 3. Response
+  //   await db.runTransaction(async t => {
+  //     // Read data from covid-latest & covid-history (in parallel)
   //     const [latestSnapshot, historySnapshot] = await Promise.all([
   //       t.get(db.collection('covid-latest').doc(stateId)),
   //       t.get(db.collection('covid-history').doc(stateId))
   //     ])
 
-  //     // Our NoSQL design requires the docs to exist already (stateId/stateName must be defined)
+  //     // Reject if US State data doesn't exist already
   //     if (!latestSnapshot.exists || !historySnapshot.exists) return res.sendStatus(404)
+  //     const stateName = latestSnapshot.data().stateName
+  //     if (!stateName) return res.sendStatus(404)
 
-  //     // Reject if this data already exists
-  //     const oldHistoryEntry = historySnapshot.data().history.find(oldHistoryEntry => oldHistoryEntry.date.seconds === date.seconds)
-  //     if (oldHistoryEntry) return res.sendStatus(409)
+  //     // Optional: Reject if data already exists for this data
+  //     // ...I prefer to just update, so that the API is "idempotent"
+  //     const history = historySnapshot.data().history
+  //     const oldHistoryEntryIndex = history.findIndex(entry => date.seconds === entry.date.seconds)
+  //     // if (data.seconds === latestSnapshot.data().date.seconds || oldHistoryEntryIndex >= 0) return res.sendStatus(409)
 
-  //     // Only update latest if request data is newer
-  //     const latestUpdate = { stateId, date, cases, casesNew, vaccineOne, vaccineOnePercent, vaccineComplete, vaccineCompletePercent }
-  //     if (latestSnapshot.data().date.seconds < date.seconds) t.set(db.collection('covid-latest').doc(stateId), latestUpdate, { merge: true })
+  //     // Replace latest (if request data is newer OR equal)
+  //     const latestUpdate = { stateId, stateName, date, cases, casesNew, vaccineOne, vaccineOnePercent, vaccineComplete, vaccineCompletePercent }
+  //     if (latestSnapshot.data().date.seconds <= date.seconds) t.set(db.collection('covid-latest').doc(stateId), latestUpdate)
 
-  //     // Always update history (add new entry)
-  //     const historyEntry = { date, cases, casesNew, vaccineOne, vaccineOnePercent, vaccineComplete, vaccineCompletePercent }
-  //     const historyUpdate = { history: firestore.FieldValue.arrayUnion(historyEntry) }
-  //     t.set(db.collection('covid-history').doc(stateId), historyUpdate, { merge: true })
-
-  //     res.sendStatus(201)
+  //     // Update history (always)
+  //     if (oldHistoryEntryIndex >= 0) {
+  //       // Update existing entry
+  //       const oldHistoryEntry = history[oldHistoryEntryIndex]
+  //       const newHistoryEntry = { ...oldHistoryEntry, date, cases, casesNew, vaccineOne, vaccineOnePercent, vaccineComplete, vaccineCompletePercent }
+  //       history[oldHistoryEntryIndex] = newHistoryEntry // update existing entry
+  //       const historyUpdate = { history } // replace entire history array
+  //       t.set(db.collection('covid-history').doc(stateId), historyUpdate, { merge: true })
+  //       res.sendStatus(200)
+  //     } else {
+  //       // Add new entry
+  //       const historyEntry = { date, cases, casesNew, vaccineOne, vaccineOnePercent, vaccineComplete, vaccineCompletePercent }
+  //       const historyUpdate = { history: firestore.FieldValue.arrayUnion(historyEntry) } // add item to array
+  //       t.set(db.collection('covid-history').doc(stateId), historyUpdate, { merge: true })
+  //       res.sendStatus(201)
+  //     }
   //   })
-
-  //   // 3. Response
-  //   await query
   // } catch (err) {
   //   console.error(err)
   //   res.sendStatus(500)
@@ -129,46 +142,44 @@ const updateCovidRecord = async (req, res) => {
     res.sendStatus(500)
   }
 
-  /**********************************/
-  /* Version 2: Check & update both */
-  /**********************************/
+  /*************************************/
+  /* Version 2: Validate & update both */
+  /*************************************/
   // try {
   //   // 1. Inputs
   //   const stateId = req.params.stateId.toUpperCase()
   //   const { date: dateMillis, cases, casesNew } = req.body
   //   const date = firestore.Timestamp.fromMillis(dateMillis)
 
-  //   // 2. Query
-  //   const query = db.runTransaction(async t => {
+  //   // 2. Query AND 3. Response
+  //   await db.runTransaction(async t => {
   //     // Read data from covid-latest & covid-history
   //     const [latestSnapshot, historySnapshot] = await Promise.all([
   //       t.get(db.collection('covid-latest').doc(stateId)),
   //       t.get(db.collection('covid-history').doc(stateId))
   //     ])
 
-  //     // Our NoSQL design requires the docs to exist already (stateId/stateName must be defined)
+  //     // Reject if US State data doesn't exist already
   //     if (!latestSnapshot.exists || !historySnapshot.exists) return res.sendStatus(404)
 
-  //     // Only update latest if request data is newer OR equal
+  //     // Reject if date is newer than latest or doesn't exist in history (ie. this isn't an update)
+  //     const history = historySnapshot.data().history
+  //     const oldHistoryEntryIndex = history.findIndex(entry => date.seconds === entry.date.seconds)
+  //     if (date.seconds > latestSnapshot.data().date.seconds || oldHistoryEntryIndex < 0) return res.sendStatus(404)
+
+  //     // Update latest (if request date equals latest date)
   //     const latestUpdate = { date, cases, casesNew }
-  //     if (latestSnapshot.data().date.seconds <= date.seconds) t.set(db.collection('covid-latest').doc(stateId), latestUpdate, { merge: true })
+  //     if (date.seconds === latestSnapshot.data().date.seconds) t.set(db.collection('covid-latest').doc(stateId), latestUpdate, { merge: true })
 
-  //     // Always update history (merge new entry into old entry)
-  //     const history = []
-  //     const historyEntry = { date, cases, casesNew } // coincidentally the same change as latestUpdate
-  //     for (const oldHistoryEntry of historySnapshot.data().history) {
-  //       if (oldHistoryEntry.date.seconds === date.seconds) history.push({ ...oldHistoryEntry, ...historyEntry })
-  //       else history.push(oldHistoryEntry)
-  //     }
-
-  //     const historyUpdate = { history }
+  //     // Update history (always)
+  //     const oldHistoryEntry = history[oldHistoryEntryIndex]
+  //     const newHistoryEntry = { ...oldHistoryEntry, date, cases, casesNew }
+  //     history[oldHistoryEntryIndex] = newHistoryEntry // update existing entry
+  //     const historyUpdate = { history } // replace entire history array
   //     t.set(db.collection('covid-history').doc(stateId), historyUpdate, { merge: true })
 
   //     res.sendStatus(200)
   //   })
-
-  //   // 3. Response
-  //   await query
   // } catch (err) {
   //   console.error(err)
   //   res.sendStatus(500)
