5ec7e86633f467a71af66239ac6c6e8b070a6f39
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-09-09 13:08:39 +0700

    [U9L] Book Shop design

--- u9-css-2/src/App.vue
+++ u9-css-2/src/App.vue
@@ -1,9 +1,73 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <b-navbar class="is-primary" wrapper-class="container">
+      <template #brand>
+        <b-navbar-item
+          tag="router-link"
+          :to="{ name: 'Home' }"
+          class="is-size-4"
+        >
+          <b-icon
+            icon="book-open-page-variant"
+            size="is-medium"
+            class="mr-3"
+          ></b-icon>
+          <span class="is-primary">BEC Books</span>
+        </b-navbar-item>
+      </template>
+
+      <template #end>
+        <b-navbar-item tag="router-link" :to="{ name: 'Home' }">
+          Search
+        </b-navbar-item>
+        <b-navbar-item
+          tag="router-link"
+          :to="{ name: 'BookDetails', params: { isbn13: '978-1593272838' } }"
+        >
+          Promo
+        </b-navbar-item>
+        <b-navbar-dropdown right>
+          <template #label>
+            <b-icon icon="cart" class="mr-1"></b-icon>
+            <span class="is-size-4 has-text-weight-medium">2</span>
+          </template>
+          <b-navbar-item
+            v-for="book in $store.getters.cart"
+            :key="book.isbn13"
+            tag="router-link"
+            :to="{ name: 'BookDetails', params: { isbn13: book.isbn13 } }"
+          >
+            <img
+              :src="`/images/books/${book.isbn13}.jpg`"
+              :alt="book.title"
+              class="mr-2"
+            />
+            <p>
+              <span class="has-text-weight-semibold">{{ book.title }}</span>
+              <br />
+              {{ book.price.toLocaleString() }} THB
+            </p>
+          </b-navbar-item>
+          <b-navbar-item class="mt-2 px-4">
+            <b-button class="is-primary" expanded>Checkout</b-button>
+          </b-navbar-item>
+        </b-navbar-dropdown>
+      </template>
+    </b-navbar>
+
+    <div class="container">
+      <router-view />
     </div>
-    <router-view/>
   </div>
 </template>
+
+<style scoped>
+.navbar-item img {
+  max-height: 3rem;
+  vertical-align: top;
+}
+
+.navbar-item p {
+  display: inline-block;
+}
+</style>
---
+++ u9-css-2/src/views/BookDetails.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="book-details">
+    <section class="section">
+      <div class="columns is-variable is-4">
+        <div
+          class="
+            column
+            has-text-centered
+            is-full is-two-fifths-tablet is-one-third-widescreen
+          "
+        >
+          <img :src="`/images/books/${book.isbn13}.jpg`" :alt="book.title" />
+        </div>
+        <div class="column">
+          <h1
+            class="is-size-2 has-text-weight-semibold has-text-centered-mobile"
+          >
+            {{ book.title }}
+          </h1>
+          <p
+            class="
+              is-size-4
+              has-text-weight-light has-text-centered-mobile
+              mt-3
+            "
+          >
+            {{ book.authors.join(', ') }}
+          </p>
+          <div class="columns is-variable is-2 mt-3">
+            <div class="column is-narrow py-2">
+              <b-button class="is-primary" size="is-medium" expanded outlined>
+                Add to Cart
+              </b-button>
+            </div>
+            <div class="column is-narrow py-2">
+              <b-button class="is-primary" size="is-medium" expanded>
+                Buy Now
+              </b-button>
+            </div>
+          </div>
+          <p class="mt-4">{{ book.description }}</p>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script lang="ts">
+import { Book } from '@/store'
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component
+export default class BookDetails extends Vue {
+  get book (): Book {
+    return this.$store.getters.bookByIsbn13(this.$route.params.isbn13)
+  }
+}
+</script>
